25190590eae19394f70796a618cf075ad854d134
Maren <maren.kjos@gmail.com>
2020-09-18 17:24:51 +0200

    (#1664) Migration script to set sensor_type:internal_calibration
    CH4 = 0
    HD16O = 1
    H218O = 1

--- src/migrations/db_migrations/V10__core_sensor_1664.sql
+++ src/migrations/db_migrations/V10__core_sensor_1664.sql
@@ -2,4 +2,8 @@
 update variable_sensors set core=1 where sensor_type=28;
 
 -- Add run_type
-update sensor_types set internal_calibration=1 where id=31;
+update sensor_types set internal_calibration=1 where id=29;
+
+update sensor_types set internal_calibration=1 where id=30;
+
+update sensor_types set internal_calibration=0 where id=31;
